bc9a4ae9262256a1941d10a0c88ffc542bd87d5b
trikimanar <trikimanar944@gmail.com>
2025-12-14 17:24:10 +0200

    comment je push it dans git

--- notes-service/src/main/java/NotesServiceApplication/java/NotesServiceApplication.java
+++ notes-service/src/main/java/NotesServiceApplication/java/NotesServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.notes_service;
+package NotesServiceApplication.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
